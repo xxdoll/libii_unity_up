d8d37cd8931c873798dc0f407e5f6d37b1408b69
Janche <957671816@qq.com>
2021-03-04 14:27:35 +0800

    :rocket: 完成前端的开发，部署

---
+++ web/src/main/resources/static/api.config.js
@@ -0,0 +1,17 @@
+// 全局配置文件，可在打包后方便修改
+window.g = {
+  // localhost
+  // baseUrl: 'http://192.168.1.201:8090/pay', // 后端服务器地址
+  // loginPageUrl: 'http://192.168.0.23:28012/#/login', // 前端登录页面地址
+  // collectionUrl: 'http://192.168.0.23:28012/#/platforms', // 整合地址，左上角得跳转
+
+  // 测试服
+  baseUrl: 'http://192.168.0.21:9090/unity_up', // 后端服务器地址
+  loginPageUrl: 'http://192.168.0.23:28012/#/login', // 前端登录页面地址
+  collectionUrl: 'http://192.168.0.23:28012/#/platforms', // 整合地址，左上角得跳转
+
+  // 正式服
+  // baseUrl: 'http://mgapi.libii.cn/ad_management', // 后端服务器地址
+  // loginPageUrl: 'https://login.libii.cn', // 前端登录页面地址
+  // collectionUrl: 'https://login.libii.cn/#/platforms', // 整合地址，左上角得跳转
+};
