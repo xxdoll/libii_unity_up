e18f78a2a8abf6c5b4737ff06d9afaaa4399e41d
hengqiubao <you@example.com>
2023-04-07 15:31:08 +0800

    前端关于配置文件上传信息提示

--- web/src/main/resources/static/api.config.js
+++ web/src/main/resources/static/api.config.js
@@ -1,14 +1,14 @@
 // 全局配置文件，可在打包后方便修改
 window.g = {
   // localhost
-   baseUrl: 'http://192.168.0.145:9980/unity_up', // 后端服务器地址
-   loginPageUrl: 'http://192.168.0.23:28012/#/login', // 前端登录页面地址
-   collectionUrl: 'http://192.168.0.23:28012/#/platforms', // 整合地址，左上角得跳转
+  // baseUrl: 'http://192.168.1.201:8090/pay', // 后端服务器地址
+  // loginPageUrl: 'http://192.168.0.23:28012/#/login', // 前端登录页面地址
+  // collectionUrl: 'http://192.168.0.23:28012/#/platforms', // 整合地址，左上角得跳转
 
   // 测试服
-//  baseUrl: 'http://192.168.0.21:9090/unity_up', // 后端服务器地址
-//  loginPageUrl: 'http://192.168.0.23:28012/#/login', // 前端登录页面地址
-//  collectionUrl: 'http://192.168.0.23:28012/#/platforms', // 整合地址，左上角得跳转
+  baseUrl: 'http://192.168.0.145:9980/unity_up', // 后端服务器地址
+  loginPageUrl: 'http://192.168.0.23:28012/#/login', // 前端登录页面地址
+  collectionUrl: 'http://192.168.0.23:28012/#/platforms', // 整合地址，左上角得跳转
 
   // 正式服
   // baseUrl: 'http://mgapi.libii.cn/ad_management', // 后端服务器地址
